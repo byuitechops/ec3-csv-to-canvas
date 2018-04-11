583a4798068547622f8afbed85a5900de60dbec9
Joshua McKinney <mckinneyj@byui.edu>
2018-04-11 01:59:23 -0600

    made question text , answer array, add questionNumber

--- readFiles.js
+++ readFiles.js
@@ -4,6 +4,7 @@ const d3 = require('d3-dsv');
 const fs = require('fs');
 const path = require('path');
 const async = require('async');
+const cheerio = require('cheerio');
 
 // Path to the files on my computer
 var folderPath = path.resolve('./CSV');
@@ -34,7 +35,7 @@ fs.readdir(folderPath, (err, items) => {
             //get the number after the V for version
             var version = fileName.match(/_V(\d)/);
             //if there was not a number keep it
-            if(version === null){
+            if (version === null) {
                 console.log('we got a null:', fileName);
                 return true;
             }
@@ -42,11 +43,11 @@ fs.readdir(folderPath, (err, items) => {
             //keep the number that was captured with the parentheses 
             version = version[1];
 
-            
+
 
             if (version === '1') {
                 //make a name that has V2 in it not V1
-                
+
                 var v2Name = fileName.replace('_V1', '_V2');
                 //console.log(v2Name);
                 //if there is a v2name in the filenames list don't keep v1
@@ -100,6 +101,7 @@ function formatQuizzes(quizzes) {
     };
 
 
+
     //add courseids and  add canvasSettings
     quizzes = quizzes
         .map(addCourseIds)
@@ -137,11 +139,73 @@ function fixQuestionData(quizzes) {
         return sortNumA - sortNumB;
     };
 
+    var makeQuestionText = (question) => {
+        //make the prop
+        questionText = '';
+
+        //add the passage if we need it
+        if (question.questionNumber === 1) {
+            //we need to remove the css link tags from the passage text
+            var $ = cheerio.load(question.passagetext);
+            $('link').remove();
+
+            //now add it to the text
+            questionText += $('body').html();
+        }
+
+        //add in the actual question
+        questionText += question.questiontext;
+
+        //make the real prop 
+        question.question_text = questionText;
+
+        return question;
+    }
+
+    var makeAnswers = (question) => {
+        var ansewrs;
+
+        if (question.skill === "reading" || question.skill === "listening") {
+            answers = [question.answertext1, question.answertext2, question.answertext3, question.answertext4]
+                .map(answer => {
+                    var hasStars = /^\*+/,
+                        //check if its a correct answer
+                        isCorrect = hasStars.test(answer);
+
+                    //remove the stars at the start    
+                    answer = answer.replace(hasStars, '');
+
+                    //send back the answer obj
+                    return {
+                        answer_text: answer,
+                        answer_weight: isCorrect ? '100' : '0'
+                    }
+                });
+        } else {
+            answers = null;
+        }
+        
+        //set it 
+        question.answers = answers;
+        
+        return question;
+    }
+
+    var addQuestionNumber = (question,i) => {
+        //add the question number/position because we sorted before we can just use the i
+        question.position = i;
+
+        return question;
+    }
+
     return quizzes.map(quiz => {
 
         quiz.questions = quiz.questions
             .map(splitQuestionName)
-            .sort(byPassageAndQuestion);
+            .sort(byPassageAndQuestion)
+            .map(makeQuestionText)
+            .map(makeAnswers)
+            .map(addQuestionNumber);
 
         return quiz;
     });
